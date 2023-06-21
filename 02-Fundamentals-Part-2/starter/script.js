82a99f747363e0b327a4de7eda0c2f2ddf4e482f
NIK Nerkar <iamp1.nik@outlook.com>
2023-06-21 23:21:27 +0530

    Functions more

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -24,5 +24,78 @@ function fruitProcessor(apples, oranges) {
 const appleJuice = fruitProcessor(2, 0);
 console.log(appleJuice);
 console.log(fruitProcessor(3, 6));
+console.log(fruitProcessor(typeof 4, typeof `four`));
+*/
+
+/*
+// FUNCTION DECLARATION
+
+function calculateAge(birthYear) {
+  return 2023 - birthYear;
+}
+const age1 = calculateAge(2001);
+console.log(age1);
+
+// FUNCTION EXPRESSION
+
+const calculateAgeAgain = function (birthYear) {
+  return 2023 - birthYear;
+};
+const age2 = calculateAgeAgain(2001);
+console.log(age2);
+
+console.log(age1, age2);
+
+*/
+
+/*
+// ARROW FUNCTION
+const calcAge3 = (birthYear) => 2023 - birthYear;
+const age3 = calcAge3(2001);
+console.log(age3);
+*/
+
+/*
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2023 - birthYear;
+  const retirementYears = 33 - age;
+  return `${firstName} retires in ${retirementYears} years!`;
+};
+
+console.log(yearsUntilRetirement(2001, `NIK`));
+*/
+
+/*
+const cutPieces = function (fruit) {
+  return fruit * 4;
+};
+
+const fruitProcessor = function (apples, oranges) {
+  const applePieces = cutPieces(apples);
+  const orangePieces = cutPieces(oranges);
+  console.log(apples, oranges);
+  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges`;
+  return juice;
+};
+console.log(fruitProcessor(3, 5));
+*/
+
+/*
+const avg = (first, second, third) => (first + second + third) / 3;
+
+const teamOneScore = avg(65, 54, 49);
+const teamTwoScore = avg(23, 65, 27);
+
+const checkWinner = function (teamOneScore, teamTwoScore) {
+  if (teamOneScore >= teamTwoScore * 2) {
+    return `TeamOne is the winner`;
+  } else if (teamTwoScore >= teamOneScore * 2) {
+    return `TeamTwo is the winner`;
+  } else {
+    return `We don't have any winner`;
+  }
+};
+
+console.log(checkWinner(teamOneScore, teamTwoScore));
 
 */
