71129ccc64f8c9f6672c3e4bff7ab762015bbff5
NIK Nerkar <iamp1.nik@outlook.com>
2023-06-25 20:01:22 +0530

    this

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,15 +1,35 @@
 'use strict';
 
-const firstName = 'NIK';
+// const firstName = 'NIK';
 
-function calcAge(birthYear) {
-  const age = 2023 - birthYear;
-  function printAge() {
-    const output = `Your are ${age}, born in ${birthYear}`;
-    console.log(output);
-  }
-  printAge();
-  return age;
-}
+// function calcAge(birthYear) {
+//   const age = 2023 - birthYear;
+//   function printAge() {
+//     const output = `${firstName} is ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-calcAge(2001);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       const str = `Oh! you're a millennial ${firstName} `;
+//       console.log(str);
+//     }
+//   }
+//   printAge();
+//   return age;
+// }
+
+// calcAge(1986);
+
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//   console.log(2023 - birthYear);
+//   console.log(this);
+// };
+
+// calcAge(2001);
+
+// const calcAgeArrow = birthYear => {
+//   console.log(2023 - birthYear);
+//   console.log(this);
+// };
+// calcAgeArrow(2001);
